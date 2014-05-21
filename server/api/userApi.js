b779dae6c0d08d3568ff740d46644c928fa73cc8
Oleg Verhovsky <oleg.verhovsky@hp.com>
2014-05-21 07:35:39 +0300

    add maing routing

--- server/api/userApi.js
+++ server/api/userApi.js
@@ -4,7 +4,10 @@ var userCtrl = require('../controllers/userCtrl.js');
 module.exports = function (app) {
 
     app.get('/user/login/:username/:password', userCtrl.login);
-
+   app.get('/', function(req , res){
+   	  var d = new Date();
+   	  res.json(d);
+   });
 
 
 }
